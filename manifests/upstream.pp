51b442f140304f7ab866df9fd1b4ec2a25417d2b
David Gillies <david.gillies@bigcommerce.com>
2015-12-10 13:30:10 +1100

    change the way we handle our upstream configs

--- manifests/upstream.pp
+++ manifests/upstream.pp
@@ -14,20 +14,18 @@ define nginx::upstream(
   $check_http_send = '',
   $check_http_expect_alive = '',
 ) {
-  
+
   validate_bool($check_health)
   validate_bool($check_default_down)
   validate_re($check_type, ['^tcp$','^http$','^ssl_hello$','^mysql$','^ajp$','^fastcgi$'])
 
   $target_dir = "/etc/nginx/upstreams.d/${name}/"
-  $target_file = "/etc/nginx/conf.d/upstream_${name}.conf"
 
   File {
     owner  => 'root',
     group  => 'root',
     mode   => '0640',
     ensure => $ensure,
-    notify => Exec["rebuild-nginx-upstream-${name}"],
   }
 
   file { $target_dir:
@@ -40,33 +38,9 @@ define nginx::upstream(
     force   => true,
   }
 
-  file { $target_file:
-    replace => false,
+  file { "/etc/nginx/conf.d/upstream_${name}.conf":
+    content => template('nginx/upstream.conf.erb'),
     notify  => Exec['reload-nginx'],
   }
 
-  file { "${target_dir}/0_header.conf":
-    content => template('nginx/upstream.header.erb'),
-  }
-
-  file { "${target_dir}/2_footer.conf":
-    content => template('nginx/upstream.footer.erb'),
-  }
-
-  $command = $ensure ? {
-    'present' => "/bin/sh -c '
-      if [ `/usr/bin/find ${target_dir} -maxdepth 1 -type f -name \"1_*.conf\" | wc -l` -gt 0 ]; then
-        /usr/bin/find ${target_dir} -maxdepth 1 -type f -name \"*.conf\" -print0 | sort -z | xargs -0 cat >| ${target_file}
-      else
-        echo '' > ${target_file}
-      fi'",
-    'absent' => '/bin/true',
-  }
-
-  exec { "rebuild-nginx-upstream-${name}":
-    command     => $command,
-    refreshonly => true,
-    require     => File[$target_dir],
-    notify      => Exec['reload-nginx'],
-  }
 }
